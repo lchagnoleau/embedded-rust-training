14ed23dea19a86be42ff8c5cec1c8d1bb80b8d33
Loïc Chagnoleau <lchagnoleau@pm.me>
2023-03-04 23:19:48 +0100

    create a blinky app

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,46 @@
-#![no_std]
+#![deny(unsafe_code)]
 #![no_main]
-
-use panic_rtt_target as _;
-use rtt_target::{rprintln, rtt_init_print};
+#![no_std]
 
 use cortex_m_rt::entry;
+use microbit::{
+    board::Board,
+    display::blocking::Display,
+    hal::{prelude::*, Timer},
+};
+use panic_rtt_target as _;
+use rtt_target::rtt_init_print;
+
+const PIXELS: [(usize, usize); 16] = [
+    (0,0), (0,1), (0,2), (0,3), (0,4), (1,4), (2,4), (3,4), (4,4),
+    (4,3), (4,2), (4,1), (4,0), (3,0), (2,0), (1,0)
+];
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
-    rprintln!("Hello World");
-    loop {}
+
+    let board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+    let mut display = Display::new(board.display_pins);
+    let mut light_it_all = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ];
+
+    let mut last_led = (0,0);
+
+    loop {
+
+        for current_led in PIXELS.iter() {
+            light_it_all[last_led.0][last_led.1] = 0;
+            light_it_all[current_led.0][current_led.1] = 1;
+            last_led = *current_led;
+
+            display.show(&mut timer, light_it_all, 30);
+        }
+    }
 }
